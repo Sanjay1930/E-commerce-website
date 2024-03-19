f14d7ac6fb50de9c753f716d11313cae14e66d50
sanjaykrishna <sanjaykrishna1930@gmail.com>
2024-03-19 20:49:48 +0530

    first commit

---
+++ backend/models/productModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:String,
+    price:String,
+    description:String,
+    ratings:String,
+    images : [{
+        image:String,
+    }],
+    categorys:String,
+    seller:String,
+    stock:String,
+    numOfReviews:String,
+    createdAt:Date
+})
+
+const productModel = mongoose.model('Product', productSchema);
+
+module.exports = productModel;
